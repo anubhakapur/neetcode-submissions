40a5b3184d50fa2862c68114a8744b02a73a787c
Anubha Kapur <114015048+anubhakapur@users.noreply.github.com>
2026-09-19 11:40:27 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+public:
+    stack<pair<int,int>>st;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(st.empty())st.push({val,val});
+        else st.push({val,min(val,st.top().second)});
+    }
+    
+    void pop() {
+        if(!st.empty())st.pop();
+    }
+    
+    int top() {
+        if(!st.empty())return st.top().first;
+        return -1;
+    }
+    
+    int getMin() {
+        if(!st.empty())return st.top().second;
+        return -1;
+    }
+};
